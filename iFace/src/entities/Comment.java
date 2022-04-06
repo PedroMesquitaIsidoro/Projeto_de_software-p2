62f6092386612e3c4ce23aa19cf5e12553b5d97c
PedroMesquitaIsidoro <83170114+PedroMesquitaIsidoro@users.noreply.github.com>
2022-04-06 13:53:31 -0300

    Create Comment.java

---
+++ iFace/src/entities/Comment.java
@@ -0,0 +1,48 @@
+
+package entities;
+
+
+public class Comment {
+    private Integer id;
+    private User user;
+    private String text;
+    
+    public Comment(){
+        super();
+    }
+    
+    public Comment(Intereger id, User user, String text){
+        super();
+        this.id = id;
+        this.user = user;
+        this.text = text;
+    }
+    
+    public Integer getId(){
+        return id;
+    }
+    
+    public void setId(Integer id){
+        this.id = id;
+    }
+    
+    public User getUser(){
+        return user;
+    }
+    
+    public void setUser(User user){
+        this.user = user;
+    }
+    
+    public String getText(){
+        return text;
+    }
+    
+    public void setText(){
+        return text;
+    }
+    
+    public void setText(String text){
+        this.text = text;
+    }
+}
