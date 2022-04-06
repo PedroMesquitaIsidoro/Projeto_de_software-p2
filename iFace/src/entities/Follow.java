a8dc997cc3c2c9b5f61c7d0e6e919829ed2baa4f
PedroMesquitaIsidoro <83170114+PedroMesquitaIsidoro@users.noreply.github.com>
2022-04-06 13:52:39 -0300

    Create Follow.java

---
+++ iFace/src/entities/Follow.java
@@ -0,0 +1,11 @@
+package entities;
+
+
+public class Follow extends User{
+    
+    public Follow (String name, String birthdate, String relationship){
+        super(name, birthdate, relationship);
+    }
+    
+    
+}
